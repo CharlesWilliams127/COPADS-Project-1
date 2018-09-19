6183fdbe494b94d10b2b0d022c7b19573bda2e9b
crw4147 <crw4147@rit.edu>
2018-09-19 18:03:11 -0400

    Added Simple Parallel Processing
    - Basic parallel processing implemented
    - Some resources will still get deadlocked, need to fix this

--- WilliamsProject1/WilliamsProject1/Program.cs
+++ WilliamsProject1/WilliamsProject1/Program.cs
@@ -15,14 +15,26 @@ namespace WilliamsProject1
         static void Main(string[] args)
         {
             // hardcode directory for now
+            // TODO: change start and end calcs to a stopwatch
             var singleCounter = new SingleThreadCounter();
             var start = DateTime.Now;
-            singleCounter.Run("C:\\");
+            singleCounter.Run("D:\\");
             var end = DateTime.Now;
-            Console.WriteLine(end - start + "s");
-            Console.WriteLine(singleCounter.FolderCount + " folders");
-            Console.WriteLine(singleCounter.FileCount + " files");
-            Console.WriteLine(singleCounter.ByteCount + " bytes");
+
+            var multiCounter = new SingleThreadCounter();
+            var multiStart = DateTime.Now;
+            multiCounter.RunParallel("D:\\");
+            var multiEnd = DateTime.Now;
+
+            Console.WriteLine("Single: " + (end - start) + "s");
+            Console.WriteLine("Single: " + singleCounter.FolderCount + " folders");
+            Console.WriteLine("Single: " + singleCounter.FileCount + " files");
+            Console.WriteLine("Single: " + singleCounter.ByteCount + " bytes");
+
+            Console.WriteLine("Parallel: " + (multiEnd - multiStart) + "s");
+            Console.WriteLine("Parallel: " + multiCounter.FolderCount + " folders");
+            Console.WriteLine("Parallel: " + multiCounter.FileCount + " files");
+            Console.WriteLine("Parallel: " + multiCounter.ByteCount + " bytes");
         }
     }
 
@@ -35,23 +47,24 @@ namespace WilliamsProject1
         private long byteCount = 0;
 
         // getters for printing in main
-        public int FolderCount {get {return folderCount; } }
+        public int FolderCount { get { return folderCount; } }
         public int FileCount { get { return fileCount; } }
         public long ByteCount { get { return byteCount; } }
 
 
         public void Run(string root)
         {
-            
+
             var dir = new DirectoryInfo(root);
 
             try
             {
                 foreach (var d in dir.GetDirectories())
                 {
-                    
+                    // TODO: if access is denied on the top level it kicks you out, fix
+                    // this isn't the case with a parallel loop since the parallel loop
+                    // will handle each directory seperately
                     folderCount++;
-                    Console.WriteLine(d.FullName);
                     Run(d.FullName);
 
                     foreach (var f in d.GetFiles())
@@ -66,5 +79,29 @@ namespace WilliamsProject1
                 Console.WriteLine(e.Message);
             }
         }
+
+        public void RunParallel(string root)
+        {
+            var dir = new DirectoryInfo(root);
+
+                Parallel.ForEach(dir.GetDirectories(), d => {
+                    try
+                    {
+                        // TODO: need to put a lock or two in here to prevent deadlock
+                        folderCount++;
+                        Run(d.FullName);
+
+                        Parallel.ForEach(d.GetFiles(), f =>
+                        {
+                            fileCount++;
+                            byteCount += f.Length;
+                        });
+                    }
+                    catch (UnauthorizedAccessException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                });
+            }
+        }
     }
-}
